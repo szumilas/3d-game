d55ca497bf93d32fff7c368d069c22e02c9222cb
szumilas <piotr.szumilas@gmail.com>
2019-01-14 22:48:17 +0100

    Mipmapping added

--- TextureManager.cpp
+++ TextureManager.cpp
@@ -20,21 +20,22 @@ int TextureManager::readTextures()
 		{
 			return 0;
 		}
-
+		
 		auto textureIdCreated = static_cast<unsigned int>(image);
-
+		
 		/* OpenGL texture binding of the image loaded by DevIL  */
 		glGenTextures(1, &textureIdCreated); /* Texture name generation */
 		glBindTexture(GL_TEXTURE_2D, textureIdCreated); /* Binding of texture name */
-
-
+		
+		
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR); /* We will use linear interpolation for magnification filter */
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR); /* We will use linear interpolation for minifying filter */
-
-
-		glTexImage2D(GL_TEXTURE_2D, 0, ilGetInteger(IL_IMAGE_BPP), ilGetInteger(IL_IMAGE_WIDTH), ilGetInteger(IL_IMAGE_HEIGHT),
-			0, ilGetInteger(IL_IMAGE_FORMAT), GL_UNSIGNED_BYTE, ilGetData());
-
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR); /* We will use linear interpolation for minifying filter */
+		
+		gluBuild2DMipmaps(GL_TEXTURE_2D, 4, ilGetInteger(IL_IMAGE_WIDTH), ilGetInteger(IL_IMAGE_HEIGHT), GL_RGBA, GL_UNSIGNED_BYTE, ilGetData());
+		
+		//glTexImage2D(GL_TEXTURE_2D, 0, ilGetInteger(IL_IMAGE_BPP), ilGetInteger(IL_IMAGE_WIDTH), ilGetInteger(IL_IMAGE_HEIGHT),
+		//	0, ilGetInteger(IL_IMAGE_FORMAT), GL_UNSIGNED_BYTE, ilGetData());
+		
 		texture.idTexture = textureIdCreated;
 		//textureIds.({ texturePath.first.textureName, { texturePath.first.textureName, texturePath.first.realWidth, texturePath.first.realHeight, textureName} });
 		ilDeleteImages(1, &textureIdCreated); /* Because we have already copied image data into texture data we can release memory used by image. */
