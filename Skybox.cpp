ada3f7365e8ffdb22290e3595d452f98d3818208
szumilas <piotr.szumilas@gmail.com>
2019-01-20 21:49:35 +0100

    Skybox top side added

--- Skybox.cpp
+++ Skybox.cpp
@@ -13,10 +13,10 @@ Skybox::Skybox(TextureManager* textureManager, float minX, float maxX, float min
 
 void Skybox::calculateXYfromRef()
 {
-	points.push_back(Point{ minX, minY, 0 });
-	points.push_back(Point{ minX, maxY, 0 });
-	points.push_back(Point{ maxX, maxY, 0 });
-	points.push_back(Point{ maxX, minY, 0 });
+	points.push_back(Point{ minX - 500, minY - 500, 0 });
+	points.push_back(Point{ minX - 500, maxY + 500, 0 });
+	points.push_back(Point{ maxX + 500, maxY + 500, 0 });
+	points.push_back(Point{ maxX + 500, minY - 500, 0 });
 }
 
 void Skybox::calculateFinalGeometry(TextureManager* textureManager)
@@ -33,10 +33,10 @@ void Skybox::calculateFinalGeometry(TextureManager* textureManager)
 		newPolygon.points.push_back({ secondPoint.x, secondPoint.y, 500 });
 		newPolygon.points.push_back({ firstPoint.x, firstPoint.y, 500 });
 
-		newPolygon.texturePoints.push_back({ 0.00f + static_cast<float>(q) / 4, 0.34f });
-		newPolygon.texturePoints.push_back({ 0.25f + static_cast<float>(q) / 4, 0.34f });
-		newPolygon.texturePoints.push_back({ 0.25f + static_cast<float>(q) / 4, 0.66f });
-		newPolygon.texturePoints.push_back({ 0.00f + static_cast<float>(q) / 4, 0.66f });
+		newPolygon.texturePoints.push_back({ 0.00f + static_cast<float>(q) / 4, 0.3333f });
+		newPolygon.texturePoints.push_back({ 0.25f + static_cast<float>(q) / 4, 0.3333f });
+		newPolygon.texturePoints.push_back({ 0.25f + static_cast<float>(q) / 4, 0.6666f });
+		newPolygon.texturePoints.push_back({ 0.00f + static_cast<float>(q) / 4, 0.6666f });
 
 		newPolygon.noOfPoints = newPolygon.texturePoints.size();
 		newPolygon.idTexture = textureManager->textures[static_cast<long>(Textures::skybox)].idTexture;
@@ -45,22 +45,43 @@ void Skybox::calculateFinalGeometry(TextureManager* textureManager)
 		polygons.push_back(newPolygon);
 	}
 
-	Polygon newPolygon;
+	{
+		//bottom
+		Polygon newPolygon;
+		newPolygon.points.push_back({ points[0].x, points[0].y, -500 });
+		newPolygon.points.push_back({ points[1].x, points[1].y, -500 });
+		newPolygon.points.push_back({ points[2].x, points[2].y, -500 });
+		newPolygon.points.push_back({ points[3].x, points[3].y, -500 });
 
-	newPolygon.points.push_back({ points[0].x, points[0].y, -500 });
-	newPolygon.points.push_back({ points[1].x, points[1].y, -500 });
-	newPolygon.points.push_back({ points[2].x, points[2].y, -500 });
-	newPolygon.points.push_back({ points[3].x, points[3].y, -500 });
+		newPolygon.texturePoints.push_back({ 0.26f, 0.00f });
+		newPolygon.texturePoints.push_back({ 0.49f, 0.00f });
+		newPolygon.texturePoints.push_back({ 0.49f, 0.32f });
+		newPolygon.texturePoints.push_back({ 0.26f, 0.32f });
 
-	newPolygon.texturePoints.push_back({ 0.26f, 0.00f });
-	newPolygon.texturePoints.push_back({ 0.49f, 0.00f });
-	newPolygon.texturePoints.push_back({ 0.49f, 0.32f });
-	newPolygon.texturePoints.push_back({ 0.26f, 0.32f });
+		newPolygon.noOfPoints = newPolygon.texturePoints.size();
+		newPolygon.idTexture = textureManager->textures[static_cast<long>(Textures::skybox)].idTexture;
+		newPolygon.color = Color{ 1,1,1 };
 
-	newPolygon.noOfPoints = newPolygon.texturePoints.size();
-	newPolygon.idTexture = textureManager->textures[static_cast<long>(Textures::skybox)].idTexture;
-	newPolygon.color = Color{ 1,1,1 };
+		polygons.push_back(newPolygon);
+	}
+	{
+		//top
+		Polygon newPolygon;
+		newPolygon.points.push_back({ points[0].x, points[0].y, 500 });
+		newPolygon.points.push_back({ points[1].x, points[1].y, 500 });
+		newPolygon.points.push_back({ points[2].x, points[2].y, 500 });
+		newPolygon.points.push_back({ points[3].x, points[3].y, 500 });
 
-	polygons.push_back(newPolygon);
+		newPolygon.texturePoints.push_back({ 0.26f, 1.00f });
+		newPolygon.texturePoints.push_back({ 0.26f, 0.6666f });
+		newPolygon.texturePoints.push_back({ 0.49f, 0.6666f });
+		newPolygon.texturePoints.push_back({ 0.49f, 1.00f });
+
+		newPolygon.noOfPoints = newPolygon.texturePoints.size();
+		newPolygon.idTexture = textureManager->textures[static_cast<long>(Textures::skybox)].idTexture;
+		newPolygon.color = Color{ 1,1,1 };
+
+		polygons.push_back(newPolygon);
+	}
 
 }
