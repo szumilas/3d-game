8dde50f08d36392d85d552d20fae4cf7ce9da421
szumilas <piotr.szumilas@gmail.com>
2020-01-05 12:55:25 +0100

    Skybox fixed

--- Skybox.cpp
+++ Skybox.cpp
@@ -9,17 +9,37 @@ Skybox::Skybox(float minX, float maxX, float minY, float maxY)
 
 	calculateXYfromRef();
 	calculateFinalGeometry();
+
+	selectable = false;
 }
 
 void Skybox::calculateXYfromRef()
 {
-	points.push_back(Point{ minX - 500, minY - 500, 0 });
-	points.push_back(Point{ minX - 500, maxY + 500, 0 });
-	points.push_back(Point{ maxX + 500, maxY + 500, 0 });
-	points.push_back(Point{ maxX + 500, minY - 500, 0 });
+	points.push_back(Point{ minX - 0, minY - 500, 0 });
+	points.push_back(Point{ minX - 0, maxY + 500, 0 });
+	points.push_back(Point{ maxX + 0, maxY + 500, 0 });
+	points.push_back(Point{ maxX + 0, minY - 500, 0 });
+
+
+	/*points.push_back(Point{ minX, minY, 0 });
+	points.push_back(Point{ minX, maxY, 0 });
+	points.push_back(Point{ maxX, maxY, 0 });
+	points.push_back(Point{ maxX, minY, 0 });
+
+	for (double q = minX; q < maxX; q += (maxX - minX) / 64)
+	{
+		points.push_back(Point{ q, minY, 0 });
+		points.push_back(Point{ q, maxY, 0 });
 	}
 
-void Skybox::calculateFinalGeometry(TextureManager* textureManager)
+	for (double q = minY; q < maxY; q += (maxY - minY) / 64)
+	{
+		points.push_back(Point{ minX, q, 0 });
+		points.push_back(Point{ maxX, q, 0 });
+	}*/
+}
+
+void Skybox::calculateFinalGeometry()
 {
 	for (size_t q = 0; q < 4; q++)
 	{
@@ -28,10 +48,10 @@ void Skybox::calculateFinalGeometry(TextureManager* textureManager)
 
 		Polygon newPolygon;
 
-		newPolygon.points.push_back({ firstPoint.x, firstPoint.y, -500 });
-		newPolygon.points.push_back({ secondPoint.x, secondPoint.y, -500 });
-		newPolygon.points.push_back({ secondPoint.x, secondPoint.y, 500 });
-		newPolygon.points.push_back({ firstPoint.x, firstPoint.y, 500 });
+		newPolygon.points.push_back({ firstPoint.x, firstPoint.y, -skyboxHeight });
+		newPolygon.points.push_back({ secondPoint.x, secondPoint.y, -skyboxHeight });
+		newPolygon.points.push_back({ secondPoint.x, secondPoint.y, skyboxHeight });
+		newPolygon.points.push_back({ firstPoint.x, firstPoint.y, skyboxHeight });
 
 		newPolygon.texturePoints.push_back({ 0.00f + static_cast<float>(q) / 4, 0.3333f });
 		newPolygon.texturePoints.push_back({ 0.25f + static_cast<float>(q) / 4, 0.3333f });
@@ -39,19 +59,75 @@ void Skybox::calculateFinalGeometry(TextureManager* textureManager)
 		newPolygon.texturePoints.push_back({ 0.00f + static_cast<float>(q) / 4, 0.6666f });
 
 		newPolygon.noOfPoints = newPolygon.texturePoints.size();
-		newPolygon.idTexture = Game::textureManager.textures[static_cast<long>(Textures::skybox)].idTexture;
+		newPolygon.idTexture = TextureManager::Instance()->textures[static_cast<long>(Textures::skybox)].idTexture;
 		newPolygon.color = Color{1,1,1};
 		
 		polygons.push_back(newPolygon);
 	}
 
+	/*for (size_t q = 0; q < 4; q++)
+	{
+		auto& firstPoint = points[4 + q];
+		auto& secondPoint = points[4 + (q + 1) % 4];
+
+		Polygon newPolygon;
+
+		newPolygon.points.push_back({ firstPoint.x, firstPoint.y, 0 });
+		newPolygon.points.push_back({ secondPoint.x, secondPoint.y, 0 });
+		newPolygon.points.push_back({ secondPoint.x, secondPoint.y, 20 });
+		newPolygon.points.push_back({ firstPoint.x, firstPoint.y, 20 });
+
+		newPolygon.texturePoints.push_back({ 0.00f, 0.00f });
+		newPolygon.texturePoints.push_back({ 1.00f, 0.00f });
+		newPolygon.texturePoints.push_back({ 1.00f, 1.00f });
+		newPolygon.texturePoints.push_back({ 1.00f, 0.00f });
+
+		newPolygon.noOfPoints = newPolygon.texturePoints.size();
+
+		newPolygon.idTexture = textureManager->textures[static_cast<long>(Textures::no_texture)].idTexture;
+		newPolygon.color = Color{ 1,1,0 };
+		polygons.push_back(newPolygon);
+	}
+
+
+	for (int q = 8; q < points.size(); q += 2)
+	{
+		auto& firstPoint = points[q];
+		auto& secondPoint = points[q + 1];
+
+		Polygon newPolygon;
+
+		newPolygon.points.push_back({ firstPoint.x, firstPoint.y, 0 });
+		newPolygon.points.push_back({ secondPoint.x, secondPoint.y, 0 });
+		newPolygon.points.push_back({ secondPoint.x, secondPoint.y, 20 });
+		newPolygon.points.push_back({ firstPoint.x, firstPoint.y, 20 });
+
+		newPolygon.texturePoints.push_back({ 0.00f, 0.00f });
+		newPolygon.texturePoints.push_back({ 1.00f, 0.00f });
+		newPolygon.texturePoints.push_back({ 1.00f, 1.00f });
+		newPolygon.texturePoints.push_back({ 1.00f, 0.00f });
+
+		newPolygon.noOfPoints = newPolygon.texturePoints.size();
+
+		newPolygon.idTexture = textureManager->textures[static_cast<long>(Textures::no_texture)].idTexture;
+
+		if((q + 8) % 16 == 0)
+			newPolygon.color = Color{ 0,1,1 };
+		else
+			newPolygon.color = Color{ 1,0,1 };
+
+		polygons.push_back(newPolygon);
+
+	}*/
+
+
 	{
 		//bottom
 		Polygon newPolygon;
-		newPolygon.points.push_back({ points[0].x, points[0].y, -500 });
-		newPolygon.points.push_back({ points[1].x, points[1].y, -500 });
-		newPolygon.points.push_back({ points[2].x, points[2].y, -500 });
-		newPolygon.points.push_back({ points[3].x, points[3].y, -500 });
+		newPolygon.points.push_back({ points[0].x, points[0].y, -skyboxHeight });
+		newPolygon.points.push_back({ points[1].x, points[1].y, -skyboxHeight });
+		newPolygon.points.push_back({ points[2].x, points[2].y, -skyboxHeight });
+		newPolygon.points.push_back({ points[3].x, points[3].y, -skyboxHeight });
 
 		newPolygon.texturePoints.push_back({ 0.26f, 0.00f });
 		newPolygon.texturePoints.push_back({ 0.49f, 0.00f });
@@ -59,7 +135,7 @@ void Skybox::calculateFinalGeometry(TextureManager* textureManager)
 		newPolygon.texturePoints.push_back({ 0.26f, 0.32f });
 
 		newPolygon.noOfPoints = newPolygon.texturePoints.size();
-		newPolygon.idTexture = Game::textureManager.textures[static_cast<long>(Textures::skybox)].idTexture;
+		newPolygon.idTexture = TextureManager::Instance()->textures[static_cast<long>(Textures::skybox)].idTexture;
 		newPolygon.color = Color{ 1,1,1 };
 
 		polygons.push_back(newPolygon);
@@ -67,10 +143,10 @@ void Skybox::calculateFinalGeometry(TextureManager* textureManager)
 	{
 		//top
 		Polygon newPolygon;
-		newPolygon.points.push_back({ points[0].x, points[0].y, 500 });
-		newPolygon.points.push_back({ points[1].x, points[1].y, 500 });
-		newPolygon.points.push_back({ points[2].x, points[2].y, 500 });
-		newPolygon.points.push_back({ points[3].x, points[3].y, 500 });
+		newPolygon.points.push_back({ points[0].x, points[0].y, skyboxHeight });
+		newPolygon.points.push_back({ points[1].x, points[1].y, skyboxHeight });
+		newPolygon.points.push_back({ points[2].x, points[2].y, skyboxHeight });
+		newPolygon.points.push_back({ points[3].x, points[3].y, skyboxHeight });
 
 		newPolygon.texturePoints.push_back({ 0.26f, 1.00f });
 		newPolygon.texturePoints.push_back({ 0.26f, 0.6666f });
@@ -78,7 +154,7 @@ void Skybox::calculateFinalGeometry(TextureManager* textureManager)
 		newPolygon.texturePoints.push_back({ 0.49f, 1.00f });
 
 		newPolygon.noOfPoints = newPolygon.texturePoints.size();
-		newPolygon.idTexture = Game::textureManager.textures[static_cast<long>(Textures::skybox)].idTexture;
+		newPolygon.idTexture = TextureManager::Instance()->textures[static_cast<long>(Textures::skybox)].idTexture;
 		newPolygon.color = Color{ 1,1,1 };
 
 		polygons.push_back(newPolygon);
--- Skybox.h
+++ Skybox.h
@@ -18,5 +18,6 @@ public:
 
 private:
 
+	float skyboxHeight = 1500;
 
 };
